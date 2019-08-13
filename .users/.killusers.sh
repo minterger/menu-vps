9741293c8f7953f8b2ebbe872c8530db2cf7a5e7
minterger <minterger@gmail.com>
2019-08-13 02:49:46 -0300

    update

--- .users/.killusers.sh
+++ .users/.killusers.sh
@@ -50,15 +50,12 @@ echo "=============================================="
 echo -e "\e[1;33m------\e[1;31mMinterger Script\e[1;33m------\e[1;32m";
 echo "=============================================="
 echo
-echo -e -n "Quiere desconectar un usuario S (si) o N (no): "
-read opcion
-case $opcion in
-  S|s)
-  bash .killusers.sh;;
-  N|n)
-  exit 1;;
-  *)
-  echo ;
-  echo -e "\e[1;31mEs una opcion invalida:";
-  exit 1;;
-esac
+echo -e -n "\e[1;32mPoner [PID] del usuario"
+echo
+read PID2
+USER2=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID2\]" | awk '{print $9}'`;
+echo "desconectando $USER2"
+kill $PID2
+echo
+echo -e "\e[1;32mPresiona una tecla para continuar...\e[1;0m"
+read foo
