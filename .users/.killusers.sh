6a6c4af7459df09e855ab37c825263d8a807382e
minterger <minterger@gmail.com>
2019-08-13 02:50:26 -0300

    update

--- .users/.killusers.sh
+++ .users/.killusers.sh
@@ -51,8 +51,8 @@ echo -e "\e[1;33m------\e[1;31mMinterger Script\e[1;33m------\e[1;32m";
 echo "=============================================="
 echo
 echo -e -n "\e[1;32mPoner [PID] del usuario"
-echo
 read PID2
+echo
 USER2=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID2\]" | awk '{print $9}'`;
 echo "desconectando $USER2"
 kill $PID2
