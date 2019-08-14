9d7ac9bb38922164a3b5f1d6f880f67157783227
minterger <minterger@gmail.com>
2019-08-14 20:40:19 -0300

    update speedtest

--- .herramientas/.speedtest.sh
+++ .herramientas/.speedtest.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-if [ -f /usr/bin/python ]; then
+speedtest () {
   echo -e "Ejecutando test de velocidad: \e[1;31m"
   echo
-  curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+  ping=$(ping -c1 google.com |awk '{print $8 $9}' |grep -v loss |cut -d = -f2 |sed ':a;N;s/\n//g;ta')
+  (
+  echo -ne "[" >&2
+  while [[ ! -e /tmp/pyend ]]; do
+  echo -ne "." >&2
+  sleep 0.8s
+  done
+  rm /tmp/pyend
+  echo -e "]" >&2
+  ) &
+  starts_test=$(python speedtest.py) && touch /tmp/pyend
+  sleep 0.6s
+  down_load=$(echo "$starts_test" | grep "Download" | awk '{print $2,$3}')
+  up_load=$(echo "$starts_test" | grep "Upload" | awk '{print $2,$3}')
+  echo "Latencia: $ping"
+  echo "Descarga: $down_load"
+  echo "Subida: $up_load"
   echo
   echo -e "\e[1;32mPresiona una tecla para continuar..."
   read foo
   exit
-else
-  clear
-fi
+}
+
 if [ -f /usr/bin/python ]; then
-  echo -e "Ejecutando test de velocidad: \e[1;31m"
-  echo
-  curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
-  echo
-  echo -e "\e[1;32mPresiona una tecla para continuar..."
-  read foo
-  exit
+  speedtest
 else
-  clear
-fi
   apt-get update >/dev/null 2>/dev/null
   apt-get install -y python >/dev/null 2>/dev/null
   echo -e "Ejecutando test de velocidad: \e[1;31m"
@@ -29,3 +36,4 @@ fi
   echo
   echo -e "\e[1;32mPresiona una tecla para continuar..."
   read foo
+fi
