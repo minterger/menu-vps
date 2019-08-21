37022bd10c7ced359e26995b5c823eeb7049d997
minterger <minterger@gmail.com>
2019-08-21 15:17:55 -0300

    update

--- .herramientas/.herramientas.sh
+++ .herramientas/.herramientas.sh
@@ -80,9 +80,39 @@ update () {
   exit 5
 }
 
+killusers () {
+  data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+  for PID in "${data[@]}"
+  do
+          #echo "check $PID";
+          NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+          USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+          IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+          if [ $NUM1 -eq 1 ]; then
+                  kill $PID;
+          fi
+  done
+
+  data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+  for PID in "${data[@]}"
+  do
+          #echo "check $PID";
+          NUM2=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+          USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+          IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+          if [ $NUM2 -eq 1 ]; then
+            kill $PID;
+          fi
+  done
+}
+
 restart () {
   echo ""
+  echo "Eperando para reiniciar"
+  killusers
+  echo ""
   echo "Reiniciando"
+  echo ""
   sudo shutdown -r now
 }
 
