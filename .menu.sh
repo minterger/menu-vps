ab031e60c1f845ea53ef817e8f7e1a6fedcb0c08
minterger <minterger@gmail.com>
2019-08-23 01:19:16 -0300

    update

--- .menu.sh
+++ .menu.sh
@@ -19,7 +19,7 @@ update () {
   wget https://raw.githubusercontent.com/minterger/menu/master/install.sh >/dev/null 2>/dev/null
   bash install.sh
   sudo rm -r install.sh >/dev/null 2>/dev/null
-  exit 1
+  exit
 }
 
 users () {
