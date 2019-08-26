fc42f7512a3162fd8abd23202be47005c25da043
minterger <minterger@gmail.com>
2019-08-26 16:22:50 -0300

    update

--- .instalacion/.squid.sh
+++ .instalacion/.squid.sh
@@ -143,12 +143,12 @@ http_access deny all
   echo "http_port 8080" >> /etc/squid/squid.conf
 
   echo "
-  coredump_dir /var/spool/squid
-  refresh_pattern ^ftp:		1440	20%	10080
-  refresh_pattern ^gopher:	1440	0%	1440
-  refresh_pattern -i (/cgi-bin/|\?) 0	0%	0
-  refresh_pattern .		0	20%	4320
-  visible_hostname proxy.mastahit.com
+coredump_dir /var/spool/squid
+refresh_pattern ^ftp:		1440	20%	10080
+refresh_pattern ^gopher:	1440	0%	1440
+refresh_pattern -i (/cgi-bin/|\?) 0	0%	0
+refresh_pattern .		0	20%	4320
+visible_hostname proxy.mastahit.com
 " >> /etc/squid/squid.conf
 
   echo
