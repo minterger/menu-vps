3f7a959cc984d7b8c9093a6d5419e378af574fd0
minterger <minterger@gmail.com>
2019-08-27 15:11:51 -0300

    update

--- .instalacion/.socks.sh
+++ .instalacion/.socks.sh
@@ -10,14 +10,14 @@ fun_socks () {
     	sockspt="\033[1;31mINDISPONIVEL"
     	var_sks1="ATIVAR SOCKS"
     fi
-    echo -e "\E[44;1;37m            GERENCIAR PROXY SOCKS             \E[0m"
+    echo -e "\E[44;1;37m            ADMINISTRAR PROXY SOCKS             \E[0m"
     echo ""
-    echo -e "\033[1;33mPORTAS\033[1;37m: \033[1;32m$sockspt"
+    echo -e "\033[1;33mPUERTAS\033[1;37m: \033[1;32m$sockspt"
     echo ""
 	echo -e "\033[1;33m[\033[1;31m1\033[1;33m] \033[1;33m$var_sks1\033[0m"
-	echo -e "\033[1;33m[\033[1;31m2\033[1;33m] \033[1;33mABRIR PORTA\033[0m"
+	echo -e "\033[1;33m[\033[1;31m2\033[1;33m] \033[1;33mABRIR PUERTA\033[0m"
 	echo -e "\033[1;33m[\033[1;31m3\033[1;33m] \033[1;33mALTERAR STATUS\033[0m"
-	echo -e "\033[1;33m[\033[1;31m0\033[1;33m] \033[1;33mVOLTAR\033[0m"
+	echo -e "\033[1;33m[\033[1;31m0\033[1;33m] \033[1;33mVOLVER\033[0m"
 	echo ""
 	echo -ne "\033[1;32mOQUE DESEJA FAZER \033[1;33m?\033[1;37m "; read resposta
 	if [[ "$resposta" = '1' ]]; then
@@ -54,13 +54,13 @@ fun_socks () {
 		    	clear
 		    	fun_conexao
 		    fi
-		    verif_ptrs
+		    #verif_ptrs
 		    fun_inisocks () {
 		    	sleep 1
-		    	var_ptsks2=$(sed -n "12 p" /etc/SSHPlus/proxy.py | awk -F = '{print $2}')
-		    	sed -i "s/$var_ptsks2/ $porta/g" /etc/SSHPlus/proxy.py
+		    	var_ptsks2=$(sed -n "12 p" ~/.Menu/.instalacion/proxy.py | awk -F = '{print $2}')
+		    	sed -i "s/$var_ptsks2/ $porta/g" ~/.Menu/.instalacion/proxy.py
 		    	sleep 1
-		    	screen -dmS proxy python /etc/SSHPlus/proxy.py
+		    	screen -dmS proxy python ~/.Menu/.instalacion/proxy.py
 		    	if grep -w "SSHPlus" /etc/rc.local > /dev/null 2>&1; then
 		    		echo ""
 		    	else
@@ -91,13 +91,13 @@ fun_socks () {
 				clear
 				fun_conexao
 			fi
-			verif_ptrs
+			#verif_ptrs
 			echo ""
 			echo -e "\033[1;32mINICIANDO O PROXY SOCKS NA PORTA \033[1;31m$porta\033[1;33m"
 			echo ""
 			abrirptsks () {
 				sleep 1
-				screen -dmS proxy python /etc/SSHPlus/proxy.py $porta
+				screen -dmS proxy python ~/.Menu/.instalacion/proxy.py $porta
 				sleep 1
 			}
 			fun_bar 'abrirptsks'
@@ -114,7 +114,7 @@ fun_socks () {
 	elif [[ "$resposta" = '3' ]]; then
 		if ps x | grep proxy.py|grep -v grep 1>/dev/null 2>/dev/null; then
 			clear
-			msgsocks=$(cat /etc/SSHPlus/proxy.py |grep -E "MSG =" | awk -F = '{print $2}' |cut -d "'" -f 2)
+			msgsocks=$(cat ~/.Menu/.instalacion/proxy.py |grep -E "MSG =" | awk -F = '{print $2}' |cut -d "'" -f 2)
 			echo -e "\E[44;1;37m             PROXY SOCKS              \E[0m"
 			echo ""
 			echo -e "\033[1;33mSTATUS: \033[1;32m$msgsocks"
@@ -127,8 +127,8 @@ fun_socks () {
 				fun_conexao
 			fi
 			fun_msgsocks () {
-				msgsocks2=$(cat /etc/SSHPlus/proxy.py |grep "MSG =" | awk -F = '{print $2}')
-				sed -i "s/$msgsocks2/ '$msgg'/g" /etc/SSHPlus/proxy.py
+				msgsocks2=$(cat ~/.Menu/.instalacion/proxy.py |grep "MSG =" | awk -F = '{print $2}')
+				sed -i "s/$msgsocks2/ '$msgg'/g" ~/.Menu/.instalacion/proxy.py
 				sleep 1
 			}
 			echo ""
@@ -142,7 +142,7 @@ fun_socks () {
 					done
 					screen -wipe > /dev/null
 					sleep 1
-					screen -dmS proxy python /etc/SSHPlus/proxy.py
+					screen -dmS proxy python ~/.Menu/.instalacion/proxy.py
 				fi
 			}
 			echo ""
