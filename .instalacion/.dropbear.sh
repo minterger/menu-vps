4c6368aa8a9cd19e364106933271b628aa081cbf
minterger <minterger@gmail.com>
2019-08-31 16:35:58 -0300

    update

--- .instalacion/.dropbear.sh
+++ .instalacion/.dropbear.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+[[ ! -d ~/.Menu ]] && rm -rf /bin/menu
+${comando[0]} > /dev/null 2>&1
+${comando[1]} > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+echo -ne "\033[1;33mESPERE \033[1;37m- \033[1;33m["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m#"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "\033[1;33mESPERE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
+
 agregarpuerto () {
   echo
   echo -n -e "\033[1;32mEscribe el puerto: \033[1;0m"
@@ -35,40 +63,40 @@ desinstalar () {
   apt-get autoremove -y >/dev/null 2>/dev/null
 }
 
-desinstalar2 () {
-  (
-  echo -ne "[" >&2
-  while [[ ! -e /tmp/instmp ]]; do
-  echo -ne "." >&2
-  sleep 0.8s
-  done
-  rm /tmp/instmp
-  echo -ne "]" >&2
-  echo
-  ) &
-  desinstala=$(desinstalar) && touch /tmp/instmp
-  sleep 0.6s
-}
+#desinstalar2 () {
+#  (
+#  echo -ne "[" >&2
+#  echo -ne "." >&2
+#  sleep 0.8s
+#  done
+#  rm /tmp/instmp
+#  echo -ne "]" >&2
+#  echo
+#  ) &
+#  while [[ ! -e /tmp/instmp ]]; do
+#  desinstala=$(desinstalar) && touch /tmp/instmp
+#  sleep 0.6s
+#}
 
 instalar () {
   apt-get update >/dev/null 2>/dev/null
   apt-get install -y dropbear >/dev/null 2>/dev/null
 }
 
-instalar2 () {
-  (
-  echo -ne "[" >&2
-  while [[ ! -e /tmp/instmp ]]; do
-  echo -ne "." >&2
-  sleep 0.8s
-  done
-  rm /tmp/instmp
-  echo -ne "]" >&2
-  echo
-  ) &
-  install=$(instalar) && touch /tmp/instmp
-  sleep 0.6s
-}
+#instalar2 () {
+#  (
+#  echo -ne "[" >&2
+#  while [[ ! -e /tmp/instmp ]]; do
+#  echo -ne "." >&2
+#  sleep 0.8s
+#  done
+#  rm /tmp/instmp
+#  echo -ne "]" >&2
+#  echo
+#  ) &
+#  install=$(instalar) && touch /tmp/instmp
+#  sleep 0.6s
+#}
 
 if [ -f /usr/sbin/dropbear ]; then
 echo -e "\033[1;32mDrobear ya esta instalado\033[0m"
@@ -79,7 +107,7 @@ case $opcion in
   s|S) clear;
   echo -e "Desinstalando Dropbear:\e[1;31m";
   echo ;
-  desinstalar2;
+  fun_bar desinstalar;
   echo ;
   echo -e "\e[1;32mDesinstalado.\n\nPresiona una tecla para continuar...";
   read foo;
@@ -91,7 +119,7 @@ case $opcion in
 esac
 else
   echo -e "\033[1;31m           Instalador Dropbear\n\033[1;37mInstalando Dropbear...\033[0m"
-  instalar2
+  fun_bar instalar
 
   echo -e "\033[1;32mInstalacion completada\033[0m"
   echo
--- .instalacion/.fast.sh
+++ .instalacion/.fast.sh
@@ -1,42 +1,70 @@
 #!/bin/bash
 
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+[[ ! -d ~/.Menu ]] && rm -rf /bin/menu
+${comando[0]} > /dev/null 2>&1
+${comando[1]} > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+echo -ne "\033[1;33mESPERE \033[1;37m- \033[1;33m["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m#"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "\033[1;33mESPERE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
+
 desinstalar () {
   snap remove fast >/dev/null 2>/dev/null
 }
 
-desinstalar2 () {
-  (
-  echo -ne "[" >&2
-  while [[ ! -e /tmp/instmp ]]; do
-  echo -ne "." >&2
-  sleep 0.8s
-  done
-  rm /tmp/instmp
-  echo -ne "]" >&2
-  echo
-  ) &
-  desinstala=$(desinstalar) && touch /tmp/instmp
-  sleep 0.6s
-}
+#desinstalar2 () {
+#  (
+#  echo -ne "[" >&2
+#  while [[ ! -e /tmp/instmp ]]; do
+#  echo -ne "." >&2
+#  sleep 0.8s
+#  done
+#  rm /tmp/instmp
+#  echo -ne "]" >&2
+#  echo
+#  ) &
+#  desinstala=$(desinstalar) && touch /tmp/instmp
+#  sleep 0.6s
+#}
 
 instalar () {
   snap install fast >/dev/null 2>/dev/null
 }
 
-instalar2 () {
-  (
-  echo -ne "[" >&2
-  while [[ ! -e /tmp/instmp ]]; do
-  echo -ne "." >&2
-  sleep 0.8s
-  done
-  rm /tmp/instmp
-  echo -ne "]" >&2
-  echo
-  ) &
-  install=$(instalar) && touch /tmp/instmp
-  sleep 0.6s
-}
+#instalar2 () {
+#  (
+#  echo -ne "[" >&2
+#  while [[ ! -e /tmp/instmp ]]; do
+#  echo -ne "." >&2
+#  sleep 0.8s
+#  done
+#  rm /tmp/instmp
+#  echo -ne "]" >&2
+#  echo
+#  ) &
+#  install=$(instalar) && touch /tmp/instmp
+#  sleep 0.6s
+#}
 
 if [ -f /snap/bin/fast ]; then
 echo -e "\033[1;32mFast ya esta instalado\033[0m"
@@ -47,7 +75,7 @@ case $opcion in
   s|S) clear;
   echo -e "Desinstalando fast:\e[1;31m";
   echo ;
-  desinstalar2;
+  fun_bar desinstalar;
   echo ;
   echo -e "\e[1;32mDesinstalado\nPresiona una tecla para continuar...";
   read foo;
@@ -59,7 +87,7 @@ case $opcion in
   esac
 else
   echo -e "\033[1;31m           Instalador Fast\n\033[1;37mInstalando Fast...\033[0m"
-  instalar2
+  fun_bar instalar
 
   echo -e "\033[1;32mInstalacion completada\033[0m"
 
