4adebd67084fad3daaa6957fd794dbcf48595cf9
minterger <minterger@gmail.com>
2019-10-20 20:52:51 -0300

    update

--- .herramientas/.banner.sh
+++ .herramientas/.banner.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-cd ~/.Menu
-echo -e "\e[32mPara salir escriba exit\e[31m"
-echo
 
 banner=$(grep "banner /home/banner.net" /etc/ssh/ssh_config)
 
 if [ "$banner" = "banner /home/banner.net" ]
-  echo "" >/dev/null 2>/dev/null
+  clear
 else
   echo "banner /home/banner.net" >>/etc/ssh/sshd_config
   service sshd reload >/dev/null 2>/dev/null
 fi
+
+cd ~/.Menu
+echo -e "\e[32mPara salir escriba exit\e[31m"
+echo
 
 echo "Minterger" >/home/banner
 
