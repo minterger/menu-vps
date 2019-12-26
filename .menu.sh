965409693d7ccc41d843251b024b69a9a09a2f9a
minterger <minterger@gmail.com>
2019-12-26 00:32:30 -0300

    update

--- .menu.sh
+++ .menu.sh
@@ -26,9 +26,9 @@ update () {
   echo -e "\e[1;0m"
   echo -e "\033[1;32mDescargando"
   echo
-  wget https://raw.githubusercontent.com/minterger/menu/master/install.sh >/dev/null 2>/dev/null
-  bash install.sh
-  sudo rm -r install.sh >/dev/null 2>/dev/null
+  ## wget https://raw.githubusercontent.com/minterger/menu/master/install.sh >/dev/null 2>/dev/null
+  ## bash install.sh
+  ## sudo rm -r install.sh >/dev/null 2>/dev/null
 
   if [ -f ~/backup ]; then
     cp ~/backup/passwd ~/.Menu/.users/
