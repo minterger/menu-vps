7184d8d80d68d5606814e199706f5c0a6af8ac71
minterger <minterger@gmail.com>
2019-12-28 16:25:40 -0300

    update

--- .users/.menuusers.sh
+++ .users/.menuusers.sh
@@ -204,7 +204,7 @@ monitorssh () {
 
   			ps x | grep [[:space:]]$user[[:space:]] | grep -v grep | grep -v pts > /tmp/tmp8
   			s1ssh="$(cat /tmp/tmp8 | wc -l)"
-  			tput setaf 3 ; tput bold ; printf '  %-14s%-21s%s\n' $user $passwd $s1ssh/$s2ssh; tput sgr0
+  			tput setaf 3 ; tput bold ; printf '  %-14s%-23s%s\n' $user $passwd $s1ssh/$s2ssh; tput sgr0
   		fi
   	done < "$database"
   	echo ""
