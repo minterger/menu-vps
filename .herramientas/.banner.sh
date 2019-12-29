4055d91ecafaeca616506f7495f5f928efb3d8af
minterger <minterger@gmail.com>
2019-12-29 02:13:19 -0300

    update

--- .herramientas/.banner.sh
+++ .herramientas/.banner.sh
@@ -18,14 +18,8 @@ while [[ condition ]]; do
   if [ "$linea" = "exit" ]
   then
 
-    sed '/exit/d' /home/banner >/home/banner.net
+    sed -i '/exit/d' /home/banner.net
 
-    if [ -f /home/banner ]; then
-      rm -r /home/banner
-      clear
-    else
-      clear
-    fi
     service sshd reload >/dev/null 2>/dev/null
     service dropbear restart >/dev/null 2>/dev/null
     echo -e "\e[0m"
