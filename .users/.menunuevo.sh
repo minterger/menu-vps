37c5bb39e13f7f0f5dc236d2cd05d1bb69895f02
minterger <minterger@gmail.com>
2020-01-05 20:27:39 -0300

    update

--- .users/.menunuevo.sh
+++ .users/.menunuevo.sh
@@ -5,19 +5,23 @@ database="/root/usuarios.db"
 echo $$ > /tmp/pids
 
 touch /tmp/usersdbr;
-data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
-for PID in "${data[@]}"
-do
-        #echo "check $PID";
-        NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
-        USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
-        IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
-        if [ $NUM1 -eq 1 ]; then
-                echo "$PID $USER" >> /tmp/usersdbr;
-        else
-                touch /tmp/usersdbr;
-        fi
-done
+basedbr () {
+  data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+  for PID in "${data[@]}"
+  do
+          #echo "check $PID";
+          NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+          USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+          IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+          if [ $NUM1 -eq 1 ]; then
+                  echo "$PID $USER" >> /tmp/usersdbr;
+          else
+                  touch /tmp/usersdbr;
+          fi
+  done
+}
+
+basedbr
 
 clear
 echo -ne "Que usuario quieres desconectar?: "
