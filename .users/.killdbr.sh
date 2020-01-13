51e2122402bf6b5542a8d21cdac5b77c24a08cf3
minterger <minterger@gmail.com>
2020-01-12 21:33:31 -0300

    update

--- .users/.killdbr.sh
+++ .users/.killdbr.sh
@@ -12,19 +12,27 @@ killmultidbr () {
   while true
   do
     rm /tmp/usersdbr
-    data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
-    for PID in "${data[@]}"
-    do
-            #echo "check $PID";
-            NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
-            USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
-            IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
-            if [ $NUM1 -eq 1 ]; then
-                    echo "$PID $USER" >> /tmp/usersdbr;
-            else
-                    touch /tmp/usersdbr;
-            fi
-    done
+    touch /tmp/usersdbr
+    data="$(ps aux | grep -i dropbear | awk '{print $2}')" ;
+    NUM3=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$data\]"`;
+    PID=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$data\]" | grep  | awk -F "[" '{print $2}' | awk -F "]" '{print $1}' | awk -F "[" '{print $2}'`;
+    USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$data\]" | awk '{print $10}'`;
+
+    echo "$PID $USER" > /tmp/usersdbr
+
+#    data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+#    for PID in "${data[@]}"
+#    do
+#            #echo "check $PID";
+#            NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+#            USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+#            IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+#            if [ $NUM1 -eq 1 ]; then
+#                    echo "$PID $USER" >> /tmp/usersdbr;
+#            else
+#                    touch /tmp/usersdbr;
+#            fi
+#    done
 
   clear
   tput setaf 7 ; tput setab 4 ; tput bold ; printf '%29s%s%-20s\n' "Dropbear Limiter"
@@ -37,7 +45,9 @@ killmultidbr () {
   		if [ -z "$user" ] ; then
   			echo "" > /dev/null
   		else
-        cat /tmp/usersdbr | grep "'$user'" | grep -v grep | grep -v pts > /tmp/tmp2
+        data="$(ps aux | grep -i dropbear | awk '{print $2}')" ;
+        cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$data\]" | grep "'$user'" | awk -F "[" '{print $2}' | awk -F "]" '{print $1}' > /tmp/tmp2
+#        cat /tmp/usersdbr | grep "'$user'" | grep -v grep | grep -v pts > /tmp/tmp2
         s1ssh="$(cat /tmp/tmp2 | wc -l)"
   			tput setaf 3 ; tput bold ; printf '  %-35s%s\n' $user $s1ssh/$s2ssh; tput sgr0
   			if [ "$s1ssh" -gt "$s2ssh" ]; then
