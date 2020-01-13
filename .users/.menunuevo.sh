14c1bad9509adc05494d9ee68a9cadb65f16300f
minterger <minterger@gmail.com>
2020-01-12 21:50:22 -0300

    update

--- .users/.menunuevo.sh
+++ .users/.menunuevo.sh
@@ -5,23 +5,23 @@ database="/root/usuarios.db"
 echo $$ > /tmp/pids
 
 touch /tmp/usersdbr;
-basedbr () {
-  data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
-  for PID in "${data[@]}"
-  do
-          #echo "check $PID";
-          NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
-          USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
-          IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
-          if [ $NUM1 -eq 1 ]; then
-                  echo "$PID $USER" >> /tmp/usersdbr;
-          else
-                  touch /tmp/usersdbr;
-          fi
-  done
-}
+#basedbr () {
+#  data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+#  for PID in "${data[@]}"
+#  do
+#          #echo "check $PID";
+#          NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+#          USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+#          IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+#          if [ $NUM1 -eq 1 ]; then
+#                  echo "$PID $USER" >> /tmp/usersdbr;
+#          else
+#                  touch /tmp/usersdbr;
+#          fi
+#  done
+#}
 
-basedbr
+#basedbr
 
 clear
 echo -ne "Que usuario quieres desconectar?: "
@@ -41,10 +41,13 @@ if [ "$user" == "$datos" ]; then
   c=1
   while [ $c -le $f ]
   do
-    cat /tmp/usersdbr | grep "'$user'" | grep -v grep | grep -v pts > /tmp/usr6
+    data="$(ps aux | grep -i dropbear | awk '{print $2}')" ;
+    cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$data\]" | grep "'$user'" | awk -F "[" '{print $2}' | awk -F "]" '{print $1}' > /tmp/user6
+#   cat /tmp/usersdbr | grep "'$user'" | grep -v grep | grep -v pts > /tmp/usr6
     ps x | grep [[:space:]]$user[[:space:]] | grep -v grep | grep -v pts > /tmp/usr5
 
-    dbr="$(cat /tmp/usersdbr | grep "'$user'" | grep -v grep | grep -v pts | awk '{print $2}' | wc -l)"
+#    dbr="$(cat /tmp/usersdbr | grep "'$user'" | grep -v grep | grep -v pts | awk '{print $2}' | wc -l)"
+    dbr="$(    cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$data\]" | grep "'$user'" | wc -l)"
     ssh="$(ps x | grep [[:space:]]$user[[:space:]] | grep -v grep | grep -v pts | awk '{print $6}' | wc -l)"
 
     if [ $dbr -gt 0 ]; then
@@ -78,7 +81,7 @@ if [ "$user" == "$datos" ]; then
     fi
 
     sleep 2s
-    basedbr
+#    basedbr
   	(( c++ ))
   done
 
