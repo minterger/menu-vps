c8bd4fc45e0aa4a3c09a977a0d89cfa33255e4bb
minterger <minterger@gmail.com>
2020-01-18 20:17:29 -0300

    update

--- .users/.fechaexp.sh
+++ .users/.fechaexp.sh
@@ -22,7 +22,7 @@ do
 
 		if [ $todate -ge $todate1 ] ;then
 			userdel --force $user > /dev/null 2>/dev/null
-      sed -i "/$user $fecha /d " /root/fechaexp.db
+      sed -i "/$user /d " /root/fechaexp.db
       sed -i "/$user /d " /root/usuarios.db
 			echo "   -=Usuario $user eliminado=-"
 		fi
