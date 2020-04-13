283346c3a70a6fc7c075f81182bc78e4abb600fe
minterger <minterger@gmail.com>
2020-04-13 01:37:13 -0300

    Update .instalar.sh

--- .instalacion/.instalar.sh
+++ .instalacion/.instalar.sh
@@ -9,12 +9,12 @@ echo
 echo -e "\e[1;31m[1]\e[1;32m Instalar Fast"
 echo -e "\e[1;31m[2]\e[1;32m Instalar DropBear"
 echo -e "\e[1;31m[3]\e[1;32m Instalar Squid"
-echo -e "\e[1;31m[4]\e[1;32m Autoinstalar Squid \e[1;33m(newbeta)"
-echo -e "\e[1;31m[5]\e[1;32m Instalar HTOP"
-echo -e "\e[1;31m[6]\e[1;32m Proxy SOCKS"
+#echo -e "\e[1;31m[4]\e[1;32m Autoinstalar Squid \e[1;33m(newbeta)"
+echo -e "\e[1;31m[4]\e[1;32m Instalar HTOP"
+echo -e "\e[1;31m[5]\e[1;32m Proxy SOCKS"
 echo -e "\e[1;31m[0]\e[1;32m Volver"
 echo
-echo -n "Seleccione una opcion [1 - 6]: "
+echo -n "Seleccione una opcion [1 - 5]: "
 read opcion
 case $opcion in
 1) clear;
@@ -32,17 +32,17 @@ echo -e "Instalando Squid:\e[1;31m";
 echo ;
 bash .squid.sh;
 echo ;;
+#4) clear;
+#echo -e "Instalando Squid:\e[1;31m";
+#echo ;
+#bash .squids.sh;
+#echo ;;
 4) clear;
-echo -e "Instalando Squid:\e[1;31m";
-echo ;
-bash .squids.sh;
-echo ;;
-5) clear;
 echo -e "Instalando HTOP:\e[1;31m";
 echo ;
 bash .htop.sh;
 echo ;;
-6) clear;
+5) clear;
 echo -e "Administrar SOCKS:\e[1;31m";
 echo ;
 bash .socks.sh;
