40bf19758b6e4404b358a98e42176e95f7656096
minterger <minterger@gmail.com>
2020-06-10 23:31:11 -0300

    update

--- .head.sh
+++ .head.sh
@@ -78,7 +78,7 @@ done <<< "${portasVAR}"
 
 # ver is badvpn esta activo
 badvpn () {
-  badvpn=`netstat -tunlp | grep bad-1 | grep tcp | awk '{print $1}'`
+  badvpn=`netstat -tunlp | grep mariadb | grep tcp | awk '{print $1}'`
 
   if [ "$badvpn" = "tcp" ]
   then
--- .herramientas/.herramientas.sh
+++ .herramientas/.herramientas.sh
@@ -2,7 +2,7 @@
 
 # cambiar mensaje de badvpn en el menu
 badvpn1 () {
-  badvpn=`netstat -tunlp | grep bad-1 | grep tcp | awk '{print $1}'`
+  badvpn=`netstat -tunlp | grep mariadb | grep tcp | awk '{print $1}'`
 
   if [ "$badvpn" = "tcp" ]
   then
@@ -15,24 +15,24 @@ badvpn1 () {
 
 # iniciar/apagar badvpn
 BadVPN () {
-pid_badvpn=$(ps x | grep bad-1 | grep -v grep | awk '{print $1}')
+pid_badvpn=$(ps x | grep mariadb | grep -v grep | awk '{print $1}')
   if [ "$pid_badvpn" = "" ]; then
     echo
     echo "Iniciando Badvpn"
     echo
-    if [[ ! -e /bin/bad-1 ]]; then
-    wget -O /bin/bad-1 https://github.com/minterger/menu-vps/raw/master/.herramientas/bad-1 &>/dev/null
-    chmod 777 /bin/bad-1
+    if [[ ! -e /bin/mariadb ]]; then
+    wget -O /bin/mariadb https://github.com/minterger/menu-vps/raw/master/.herramientas/mariadb &>/dev/null
+    chmod 777 /bin/mariadb
     fi
-    screen -dmS screen /bin/bad-1 --listen-addr 127.0.0.1:3306 --max-clients 1000 --max-connections-for-client 10
-    [[ "$(ps x | grep bad-1 | grep -v grep | awk '{print $1}')" ]] && echo "Inciado" || echo "Fallo"
+    screen -dmS screen /bin/mariadb --listen-addr 127.0.0.1:3306 --max-clients 1000 --max-connections-for-client 10
+    [[ "$(ps x | grep mariadb | grep -v grep | awk '{print $1}')" ]] && echo "Inciado" || echo "Fallo"
     echo
   else
     echo
     echo "Parando Badvpn"
-    kill -9 $(ps x | grep bad-1 | grep -v grep | awk '{print $1'}) > /dev/null 2>&1
-    killall bad-1 > /dev/null 2>&1
-    [[ ! "$(ps x | grep bad-1 | grep -v grep | awk '{print $1}')" ]] && echo -e "${cor[4]} ${txt[125]}"
+    kill -9 $(ps x | grep mariadb | grep -v grep | awk '{print $1'}) > /dev/null 2>&1
+    killall mariadb > /dev/null 2>&1
+    [[ ! "$(ps x | grep mariadb | grep -v grep | awk '{print $1}')" ]] && echo -e "${cor[4]} ${txt[125]}"
     unset pid_badvpn
     fi
 unset pid_badvpn
